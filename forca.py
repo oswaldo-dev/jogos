a70a6271b9e86158e565540ff89d6c7c1b87abe3
oswaldo-dev <oswaldo.dev.oliveira@gmail.com>
2022-03-26 18:34:06 -0300

    Até aqui eu consegui o jogo adivinhacao.py esta funcionando bem como esperado, agora estou melhorando o jogo da forca.py Estou estudando a lógica do python e até agora isso é o q eu aprendi.

---
+++ forca.py
@@ -0,0 +1,43 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo no jogo de Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]  # List Comprehensions
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    # enquanto nao enforcou (enforcou = true) e nao acertou (acertou = true) continua jogando
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você errou!")
+    print("Fim do jogo!")
+
+
+if (__name__ == "__main__"):
+    jogar()
